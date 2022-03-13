240c7964102c601ec6de84a2c47b7904dc8a7d19
Andrew Piper <andrew@threax.com>
2022-03-13 12:56:59 -0400

    Push tags too.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,8 @@
 git push
+git push --tags
 git push https://github.com/threax/Threax.Tests.git
+git push https://github.com/threax/Threax.Tests.git --tags
 git push https://gitlab.com/threax-software/Threax.Tests.git
+git push https://gitlab.com/threax-software/Threax.Tests.git --tags
 git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Tests
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Tests --tags
