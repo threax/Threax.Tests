2b0b29f0cf9ca1671ccea3f29c686c454c965173
Andrew Piper <andrew@threax.com>
2022-03-13 12:45:41 -0400

    Bump to full 3.0.0. Add push file.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/Threax.Tests.git
+git push https://gitlab.com/threax-software/Threax.Tests.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Tests
